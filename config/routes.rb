d8e6f750830490d22a0100cc455f6b608a3ba6e8
terukouki07 <azumacha08@gmail.com>
2024-01-07 05:39:27 +0000

    [finish]deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #会員側のsign_id
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  #管理者側のsign_in
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
