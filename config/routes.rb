6cce10f8d39f8d183871f103ee1a15b61f93b6f9
terukouki07 <azumacha08@gmail.com>
2024-01-07 06:57:22 +0000

    [finish]rootes作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,29 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :public do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'chats/show'
-  end
-  namespace :public do
-    get 'favorites/index'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
   #会員側のsign_id
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
-  #管理者側のsign_in
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    resources :posts do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorite, only: [:create, :index, :destroy]
+    end
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :chats, only: [:create, :show]
+  end
+  
+  #管理者側のルーティング
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    resources :genres, only: [:create, :index, :edit, :update, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
