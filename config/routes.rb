dbfd7f46ce240754931458c9c4ca74564472cdc3
terukouki07 <azumacha08@gmail.com>
2024-01-07 06:26:45 +0000

    [finish]controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :public do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'chats/show'
+  end
+  namespace :public do
+    get 'favorites/index'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   #会員側のsign_id
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
