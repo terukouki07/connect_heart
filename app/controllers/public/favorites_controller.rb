f578c49b82366dd4f315263c7284a54ffa5a34bd
terukouki07 <azumacha08@gmail.com>
2024-02-05 15:07:02 +0000

    [add]いいね非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,18 +1,15 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = Favorite.new(customer_id: current_customer.id, post_id: @post.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    #current_customerがしているいいねの情報を取り出し、その中にpost.idがあるか探す。あれば取得。なければ空。
-    favorite = current_customer.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = Favorite.find_by(customer_id: current_customer.id, post_id: @post.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
